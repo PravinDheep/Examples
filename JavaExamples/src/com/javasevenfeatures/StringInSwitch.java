d0a66010790ceebcd05465d9905580aa42812f82
PravinDheep <pradeepharidass@gmail.com>
2022-02-04 11:45:25 +0530

    Updating switch case.

--- JavaExamples/src/com/javasevenfeatures/StringInSwitch.java
+++ JavaExamples/src/com/javasevenfeatures/StringInSwitch.java
@@ -5,13 +5,13 @@ public class StringInSwitch {
 	//Java 7 features
 	
 	public static void main(String[] args) {
-		String param = "java6";	
+		String param = "java7";	
 		switch (param) {
-		case "java5":
-				System.out.println("java 5");
-				break;
 		case "java6":
 				System.out.println("java 6");
+				break;
+		case "java7":
+				System.out.println("java 7");
 				break;
 			default:
 				System.out.println("default");
